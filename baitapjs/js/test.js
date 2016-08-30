766d030731fb0231524d71ce6fa0d9807b2f24df
tuananh1992 <hoanghanu92@gmail.com>
2016-08-30 15:16:34 +0700

    tinhtoanjs

---
+++ baitapjs/js/test.js
@@ -0,0 +1,21 @@
+
+function myFunction() {
+	var a = parseInt(document.getElementById('number1').value);
+	var b = parseInt(document.getElementById('number2').value);
+	alert(a + b);
+}
+function myFunction1() {
+	var a = parseInt(document.getElementById('number1').value);
+	var b = parseInt(document.getElementById('number2').value);
+	alert(a - b);
+}
+function myFunction2() {
+	var a = parseInt(document.getElementById('number1').value);
+	var b = parseInt(document.getElementById('number2').value);
+	alert(a * b);
+}
+function myFunction3() {
+	var a = parseInt(document.getElementById('number1').value);
+	var b = parseInt(document.getElementById('number2').value);
+	alert(a/b);
+}
