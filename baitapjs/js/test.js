0bc62e790081737df26e881f82bd15bf87b0e82e
tuananh1992 <hoanghanu92@gmail.com>
2016-08-30 16:38:25 +0700

    form register

--- baitapjs/js/test.js
+++ baitapjs/js/test.js
@@ -19,3 +19,15 @@ function myFunction3() {
 	var b = parseInt(document.getElementById('number2').value);
 	alert(a/b);
 }
+
+function myFuction4() {
+	document.getElementById('myForm').reset();
+}
+
+function register() {
+	var username = document.getElementById('Username').value;
+	var password = document.getElementById('Password').value;
+	var firstname = document.getElementById('Firstname').value;
+	var lastname = document.getElementById('Lastname').value; 
+	alert("Username : " + username + "\r\n Password : " + password + "\r\n Firstname : "+ firstname + "\r\n Lastname : " +  lastname);
+}
